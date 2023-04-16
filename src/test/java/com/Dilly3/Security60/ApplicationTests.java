ef7802608228ef53f7188d4daa7fc05929b1c56e
Anikammadu michael <dil.anikamadu@gmail.com>
2023-04-16 08:48:38 +0100

    project setup

---
+++ src/test/java/com/Dilly3/Security60/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Dilly3.Security60;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
